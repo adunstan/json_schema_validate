2eccbb510749d1f97a2c6260661267411bebfea1
Andrew Dunstan <andrew@dunslane.net>
2026-02-26 09:20:53 -0500

    Add PostgreSQL License

--- json_schema_validate.c
+++ json_schema_validate.c
@@ -3,6 +3,23 @@
  * json_schema_validate.c
  *    JSON Schema validation for PostgreSQL
  *
+ * Copyright (c) 2026, Andrew Dunstan
+ *
+ * Permission to use, copy, modify, and distribute this software and its
+ * documentation for any purpose, without fee, and without a written agreement
+ * is hereby granted, provided that the above copyright notice and this
+ * paragraph appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ *-------------------------------------------------------------------------
+ *
  * This extension provides functions to validate JSON/JSONB data against
  * JSON Schema specifications. It supports a significant subset of JSON
  * Schema draft-07, including:
